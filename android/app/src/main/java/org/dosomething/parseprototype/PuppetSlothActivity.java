e1f7e579a4ca00df003e64e43e35b5f7fa0d68f5
Jon Uy <jonxuy@gmail.com>
2015-05-18 15:11:17 -0400

    Added PuppetSlothActivity

---
+++ android/app/src/main/java/org/dosomething/parseprototype/PuppetSlothActivity.java
@@ -0,0 +1,16 @@
+package org.dosomething.parseprototype;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PuppetSlothActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puppet_sloth);
+    }
+}
